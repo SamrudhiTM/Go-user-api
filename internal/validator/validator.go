ed1398090092c40e7706473a9c7b401d5a54bb51
unknown <samrudhisamri28@gmail.com>
2025-12-18 18:14:58 +0530

    Updated validator logic

--- internal/validator/validator.go
+++ internal/validator/validator.go
@@ -1,9 +1,12 @@
 package validator
 
 import (
+
 	"fmt"
+	"time"
 
 	"github.com/go-playground/validator/v10"
+	"github.com/SamrudhiTM/user_api/internal/models"
 )
 
 // Create a single validator instance
@@ -13,6 +16,17 @@ var validate = validator.New()
 func ValidateStruct(s interface{}) map[string]string {
 	err := validate.Struct(s)
 	if err == nil {
+		// Extra custom checks
+		if req, ok := s.(models.CreateUserRequest); ok {
+			errors := make(map[string]string)
+			dob, parseErr := time.Parse("2006-01-02", req.Dob)
+			if parseErr == nil {
+				if dob.After(time.Now()) {
+					errors["Dob"] = "Dob cannot be in the future"
+					return errors
+				}
+			}
+		}
 		return nil
 	}
 
@@ -26,11 +40,23 @@ func ValidateStruct(s interface{}) map[string]string {
 			msg = fmt.Sprintf("%s is required", fieldName)
 		case "datetime":
 			msg = fmt.Sprintf("%s must be in YYYY-MM-DD format", fieldName)
+		case "min":
+			msg = fmt.Sprintf("%s must be at least %s characters", fieldName, err.Param())
+		case "max":
+			msg = fmt.Sprintf("%s must be at most %s characters", fieldName, err.Param())
 		default:
 			msg = fmt.Sprintf("%s is invalid", fieldName)
 		}
 
 		errors[fieldName] = msg
+	}
+
+	// ✅ Custom future DOB check
+	if req, ok := s.(models.CreateUserRequest); ok {
+		dob, parseErr := time.Parse("2006-01-02", req.Dob)
+		if parseErr == nil && dob.After(time.Now()) {
+			errors["Dob"] = "Dob cannot be in the future"
+		}
 	}
 
 	return errors
